73e143a6b97c639599eea9e5ffa1aca870de61e9
陈建 <331006361@qq.com>
2025-10-04 15:54:49 +0800

    语言切换按钮提交

--- src/views/comments/Header.vue
+++ src/views/comments/Header.vue
@@ -13,6 +13,19 @@
         </div>
         <div class="menu-icon">
           <img :src="MenuIcon" alt="menu" />
+          <span
+            class="language"
+            @click="changeLanguage('en-US')"
+            :class="{ 'language-active': locale === 'en-US' }"
+            >EN</span
+          >
+          <span class="language-separator">|</span>
+          <span
+            class="language"
+            @click="changeLanguage('zh-CN')"
+            :class="{ 'language-active': locale === 'zh-CN' }"
+            >中文</span
+          >
         </div>
       </div>
       <div class="header-right-bottom">
@@ -34,6 +47,14 @@
 import Logo from "@/assets/images/logo.svg";
 import MenuIcon from "@/assets/images/header/menu_icon.svg";
 import Touch from "@/assets/images/header/touch.svg";
+import { useI18n } from "vue-i18n";
+
+const { locale } = useI18n();
+
+const changeLanguage = (key) => {
+  locale.value = key;
+  localStorage.setItem("locale", key);
+};
 </script>
 
 <style scoped lang="less">
@@ -85,9 +106,25 @@ import Touch from "@/assets/images/header/touch.svg";
         display: flex;
         align-items: center;
         justify-content: center;
+        gap: 5px;
         img {
-          width: 72px;
           height: 15px;
+          margin-right: 15px;
+        }
+        .language {
+          color: #fff;
+          font-size: 14px;
+          font-weight: 600;
+          cursor: pointer;
+        }
+        .language-separator {
+          color: #fff;
+          font-size: 14px;
+          font-weight: 600;
+          cursor: pointer;
+        }
+        .language-active {
+          color: #8d4cfe;
         }
       }
     }
